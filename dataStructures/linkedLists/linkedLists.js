10b12eaa2a7667d53513cb0c8d034ba8d985bfb0
Pantelis <pantpap@gmail.com>
2022-06-09 00:45:58 +0300

    feat(linkedLists) create LinkedList class

--- dataStructures/linkedLists/linkedLists.js
+++ dataStructures/linkedLists/linkedLists.js
@@ -4,3 +4,12 @@ class Node{
         this.next = null;
     }
 }
+
+class LinkedList {
+    constructor(value){
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+        this.length = 1;
+    }
+}
