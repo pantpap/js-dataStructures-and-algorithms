a9f075f10f1d5aa4605e5161ef49688cc719fc62
Pantelis Papasavvas <pantpap@gmail.com>
2022-06-12 18:18:50 +0300

    feat(doublyLinkedLists) remove method

--- doublyLInkedLists.js
+++ doublyLInkedLists.js
@@ -118,4 +118,19 @@ class DoublyLinkedList {
         return true;
     }
 
+    remove(index) {
+        if(index === 0) return this.shift();
+        if(index === this.length - 1) return this.pop();
+        if(index < 0 || index >= this.length) return undefined;
+
+        const temp = this.get(index);
+
+        temp.prev.next = temp.next;
+        temp.next.prev = temp.prev;
+        temp.prev = null;
+        temp.next = null;
+        this.length--;
+        return temp;
+    }
+
 }
