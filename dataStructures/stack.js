c2bb1ce2095af068a2433c2bf9b0b537ccb98647
Pantelis Papasavvas <pantpap@gmail.com>
2022-06-13 12:02:45 +0300

    stack push method

--- dataStructures/stack.js
+++ dataStructures/stack.js
@@ -11,6 +11,17 @@ class Stack {
         this.top = newNode;
         this.length = 1;
     }
+
+    push(value){
+        if(this.length === 0){
+            this.top = newNode;
+        } else {
+            newNode.next = this.top;
+            this.top = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
 
 let myStack = new Stack(11);
