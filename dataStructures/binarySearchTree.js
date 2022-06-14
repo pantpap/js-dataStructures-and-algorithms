25b38269b8cc74f8ee56642a3d7b0180be2be5fa
Pantelis Papasavvas <pantpap@gmail.com>
2022-06-14 11:13:16 +0300

    binarySearchTree insert method

--- dataStructures/binarySearchTree.js
+++ dataStructures/binarySearchTree.js
@@ -10,6 +10,41 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        } 
+        let temp = this.root;
+        while (true) {
+            if (newNode.value === temp.value) return undefined;
+            if(newNode.value < temp.value){
+                if(temp.left === null){
+                    temp.left = newNode;
+                    return this;
+                }
+                temp = temp.left;
+            } else {
+                if(temp.right === null){
+                    temp.right = newNode;
+                    return this;
+                }
+                temp = temp.right;
+            }
+        }
+
+        // let myTree = new BinarySearchTree();
+        // myTree.insert(47)
+        // myTree.insert(21)
+        // myTree.insert(76)
+        // myTree.insert(18)
+
+        // myTree.insert(52)
+        // myTree.insert(82)
+        //  after creation we will insert newNode 27 and will be below 18
+    }
 }
 
 let myTree = new BinarySearchTree();
