1d57edf6e56d724819f4a5077d7734c310f62cfa
Pantelis Papasavvas <pantpap@gmail.com>
2022-07-03 19:37:25 +0300

    graphs addEdge

--- dataStructures/graphs.js
+++ dataStructures/graphs.js
@@ -10,4 +10,13 @@ class Graph {
         }
         return false;
     }
+
+    addEdge(vertex1, vertex2){
+        if(this.adjacencyList[vertex1] && this.adjacencyList[vertex2]){
+            this.adjacencyList[vertex1].push(this.adjacencyList[vertex2]);
+            this.adjacencyList[vertex2].push(this.adjacencyList[vertex1]);
+            return true;
+        }
+        return false;
+    }
 }
