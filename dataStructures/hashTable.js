f61ae33fbde3d6d15b009f168b2bb6a4c453f927
ppapasavvas <papasavvas.panteleimon@nbg.gr>
2022-07-05 20:43:14 +0300

    feat(hashTables) keys function

--- dataStructures/hashTable.js
+++ dataStructures/hashTable.js
@@ -31,6 +31,19 @@ class HashTable {
         }
         return undefined
     }
+
+    keys(){
+        let allKeys = [];
+        for(let i = 0; i < this.dataMap.length; i++){
+            if(this.dataMap[i].length){
+                for(let j=0; j < this.dataMap[i].length; j++){
+                    allKeys.push(this.dataMap[i][j][0])
+                }
+            }
+        }
+        return allKeys;
+    }
+    
     
 }
 
